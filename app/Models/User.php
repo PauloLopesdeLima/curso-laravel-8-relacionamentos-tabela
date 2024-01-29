5586f2853af91130b0b6669b7953e4cdc70cbc01
PauloLopesdeLima <zecariocaofc.com>
2024-01-29 16:20:20 -0300

    Evolution 3

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,6 +40,10 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
+
+    public function preference()
+    {
+        return $this->hasOne(Preference::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,33 @@
 <?php
 
+use App\Models\{
+    User,
+    Preference,
+};
 use Illuminate\Support\Facades\Route;
+
+Route::get('/one-to-one', function () {
+    $user = User::find(2);
+
+    $data = [
+        'background_color' => '#fff',
+    ];
+
+    if($user->preference) {
+        $user->preference->update($data);
+    } else {
+        // $user->preference()->create($data);
+        $preference = new Preference($data);
+        $user->preference()->save($preference);
+    }
+
+    $user->refresh();
+    var_dump($user->preference);
+
+    $user->preference->delete();
+    $user->refresh();
+    dd($user->preference);
+});
 
 Route::get('/', function () {
     return view('welcome');
